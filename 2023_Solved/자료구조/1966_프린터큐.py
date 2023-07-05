81dcfc0d9f7ffb37ab9218a0b2e7dc8673ecd13b
seonye <kss02281@gmail.com>
2023-07-05 19:45:16 +0900

    1966

--- 2023_Solved/자료구조/1966_프린터큐.py
+++ 2023_Solved/자료구조/1966_프린터큐.py
@@ -2,4 +2,27 @@
 https://www.acmicpc.net/problem/1966
 난이도 : 실버 3
 '''
+import sys
+from collections import deque
+input = sys.stdin.readline
 
+t = int(input())
+
+for _ in range(t):
+    n, m = map(int, input().split())
+    p_arr = list(map(int, input().split()))
+    priority = deque(zip([i for i in range(n)], p_arr))
+    cnt = 0
+    while True:
+        idx, p = priority.popleft()
+        if idx == m and p == max(p_arr):
+            cnt += 1
+            print(cnt)
+            break
+        elif idx != m and p == max(p_arr):
+            p_arr.remove(p)
+            cnt += 1
+        else:
+            priority.append((idx, p))
+        #print(p_arr, list(priority))
+            
---
+++ 2023_Solved/자료구조/2346_풍선터뜨리기.py
@@ -0,0 +1,4 @@
+'''
+https://www.acmicpc.net/problem/2346
+난이도 : 실버3
+'''
