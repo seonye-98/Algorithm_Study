832cf56501b7c23586ea4332757d08f3118648cd
seonye-98 <kss02281@gmail.com>
2023-08-21 16:36:04 +0900

    greedy

---
+++ 2023_JS_Practice/Algorithm/Greedy/greedy1.js
@@ -0,0 +1,6 @@
+// 그리디 문제 풀이1
+//https://www.acmicpc.net/problem/11047
+
+const path = require('path');
+const fs = require('fs');
+const input = fs.readFileSync(path.join(__dirname, 'dev', 'stdin')).toString().split('\n');
